9cc2b145339a177c637e858eac2d6828710c638a
Gage <gcm55@gmail.com>
2016-12-08 15:24:05 -0700

    Added include and definitions for shader and Width/Height

---
+++ ezview.c
@@ -0,0 +1,30 @@
+/*--------------------------------------
+CS 430 - Project 5: Image Viewer
+File: ezview.c
+Author: Gage Myers
+-----------------------------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GLFW_INCLUDE_ES2
+#include <GLFW/glfw3.h>
+
+static const GLuint WIDTH = 800;
+static const GLuint HEIGHT = 600;
+static const GLchar* vertex_shader_source =
+    "#version 100\n"
+    "attribute vec3 position;\n"
+    "void main() {\n"
+    "   gl_Position = vec4(position, 1.0);\n"
+    "}\n";
+static const GLchar* fragment_shader_source =
+    "#version 100\n"
+    "void main() {\n"
+    "   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "}\n";
+static const GLfloat vertices[] = {
+        0.0f,  0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+};
